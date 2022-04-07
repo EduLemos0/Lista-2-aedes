ea713574ba4875b32d7447bf3fa476e13ce42748
EduLemos0 <103197804+EduLemos0@users.noreply.github.com>
2022-04-07 11:42:14 -0300

    Create exercício 9.C

---
+++ exercício 9.C
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+// Devemos criar um código que faça a sequência de fibonacci quantas vezes o
+// usuário desejar.
+
+int main(void) {
+  int rep = 0, fib = 0, num1 = 0, num2 = 1;
+  printf("Digite quantos números vão participar da sequência: \n");
+  scanf("%d", &rep);
+  for (int x = 1; fib <= rep; x++) {
+    if (x == 1) {
+      printf("%d\n", num1);
+    } else if (x == 2) {
+      printf("%d\n", num2);
+    } else {
+      fib = num1 + num2;
+      num1 = num2;
+      num2 = fib;
+      if(fib < rep); // encontrará o número mais próximo do número dado.
+      printf("%d\n", fib);
+    }
+  }
+}
