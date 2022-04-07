49c08dbe1a1940a150c60f1de52bd0239f2cfa1a
EduLemos0 <103197804+EduLemos0@users.noreply.github.com>
2022-04-07 11:17:33 -0300

    Update and rename exercício 4 to exercício 4.C

--- exercício 4.C
+++ exercício 4.C
@@ -2,15 +2,15 @@
 
 int main(void) {
   int N = 0;
-  double fat = 1, E = 0;
+  double fat = 1, E = 1;
 
   printf("Digite quantos números terá a sequência: "); //N limita o número de repetições dentro do for.
   scanf("%d",&N);
   
   for(int x=1;x <= N; x++){
-    E += 1/fat; 
     fat *= x;
+    E += 1/fat; 
     printf("%d %lf %lf \n", x, fat,E);
   }
 
-}
+} 
