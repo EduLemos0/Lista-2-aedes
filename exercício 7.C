02c05f5016443f78d4b0afcd1d4c785fe1f13ab4
EduLemos0 <103197804+EduLemos0@users.noreply.github.com>
2022-04-07 11:40:55 -0300

    Update exercício 7.C

--- exercício 7.C
+++ exercício 7.C
@@ -1,15 +1,22 @@
 #include <stdio.h>
 
-//Devemos criar um código que faça a sequência de fibonacci quantas vezes o usuário desejar.
+// Devemos criar um código que faça a sequência de fibonacci quantas vezes o
+// usuário desejar.
 
 int main(void) {
-  int rep=0, fib=0, num1=0, num2=1;
+  int rep = 0, fib = 0, num1 = 0, num2 = 1;
   printf("Digite quantos números vão participar da sequência: \n");
-  scanf("%d",&rep);
-  for(int x = 0;x < rep; x++){
-    fib = num1 + num2;
-    num1 = num2;
-    num2 = fib;
-    printf("%d\n",fib);
+  scanf("%d", &rep);
+  for (int x = 1; fib <= rep; x++) {
+    if (x == 1) {
+      printf("%d\n", num1);
+    } else if (x == 2) {
+      printf("%d\n", num2);
+    } else {
+      fib = num1 + num2;
+      num1 = num2;
+      num2 = fib;
+      printf("%d\n", fib);
+    }
   }
 }
